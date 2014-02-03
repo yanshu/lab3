c98334b8bc15efb5871a46437080d1e063a6ab0d
FeifeiHuang <huangfeifei91@gmail.com>
2014-02-03 18:45:41 -0500

    change file name to lab3_2a

---
+++ lab3_2a.jl
@@ -0,0 +1,9 @@
+include("path/lab3_2a.jl");
+function printAscii(x::Array)
+    x = uint8(x)
+    @printf(STDOUT,"%s",ascii(x))
+end
+
+x = 127*rand(1024);	#create an array of random number from 0 to 127
+printAscii(x);
+print ("\n", @elapsed printAscii(x));
