a3ee04bf52ecbe0bbd369d82baf6796f0a6a9bcd
FeifeiHuang <huangfeifei91@gmail.com>
2014-02-03 18:24:47 -0500

    problem(2b). test.

--- lab3.jl
+++ lab3.jl
@@ -1,2 +1,9 @@
-include("path/newfile.jl");
-x = Array(1024,Float64)
+include("path/lab3.jl");
+function printAscii(x::Array)
+    x = uint8(x)
+    print(ascii(x), "\n")
+end
+
+x = 55*ones(1024);
+printAscii(x);
+print (@elapsed printAscii(x));
