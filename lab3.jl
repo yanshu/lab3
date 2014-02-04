3b69cc772cd39f1ba98a0134d6b052eafb2ced0e
FeifeiHuang <huangfeifei91@gmail.com>
2014-02-03 19:08:32 -0500

    script for problem2(b)

--- lab3.jl
+++
@@ -1,9 +0,0 @@
-include("path/lab3.jl");
-function printAscii(x::Array)
-    x = uint8(x)
-    @printf(STDOUT,"%s",ascii(x))
-end
-
-x = 127*rand(1024);	#create an array of random number from 0 to 127
-printAscii(x);
-print ("\n", @elapsed printAscii(x));
---
+++ lab3_2b.jl
@@ -0,0 +1,20 @@
+include"path/lab3_2b.jl"
+
+function printAscii(x::Array)
+    x = uint8(x);
+    @printf(STDOUT,"%s",ascii(x));
+end
+
+function writeASCII(x::Array,file_name)
+	open(file_name,"r+);
+	x = uint8(x);
+	x = ascii(x);
+	writecsv(file_name,x);
+end
+
+function writeArray(x::Array,file_name)
+	open(file_name,"r+");
+	writecsv(file_name,x);
+end
+
+x = 127*rand(1024);
