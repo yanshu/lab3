94c6a9e6b6302b2eb7be3865d373d8070196d93f
yanshu <huangfeifei91@gmail.com>
2014-02-04 15:53:23 -0500

    change to two functions

--- lab3_2a.jl
+++ lab3_2a.jl
@@ -1,9 +1,9 @@
 include("path/lab3_2a.jl");
-function printAscii(x::Array)
+function convertToAscii(x::Array)
     x = uint8(x)
-    @printf(STDOUT,"%s",ascii(x))
+    return ascii(x)
 end
 
 x = 127*rand(1024);	#create an array of random number from 0 to 127
-printAscii(x);
+@printf(STDOUT,"%s",convertToAscii(x))
 print ("\n", @elapsed printAscii(x));
