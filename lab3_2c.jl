0d454de1ef9a75fd2a354c52b1cdbc9cff173372
yanshu <huangfeifei91@gmail.com>
2014-02-04 20:54:51 -0500

    problem2c

---
+++ lab3_2c.jl
@@ -0,0 +1,14 @@
+include("/Users/feifeihuang/lab3/lab3_2a.jl");
+include("/Users/feifeihuang/lab3/lab3_2b.jl");
+
+function getArrayAndTime()
+    x = 127*rand(1024);	#create an array of random number from 0 to 127
+    x = convertToAscii(x);
+    file_name = "/Users/feifeihuang/lab3/file_ascii.txt";
+    println("\n","time to print ASCII to STDOUT = ", @elapsed printSTDOUT(x));
+    println("\n","time to write ASCII to file = ", @elapsed writeASCII(x,file_name));
+    println("\n","time to read ASCII = ", @elapsed readASCII(file_name));
+
+end
+
+getArrayAndTime()
