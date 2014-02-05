9ba9e531d6ce316973a53eee6ed2876bb36d622b
yanshu <huangfeifei91@gmail.com>
2014-02-04 20:39:00 -0500

    update

--- lab3_2b.jl
+++ lab3_2b.jl
@@ -1,23 +1,12 @@
-include("/Users/feifeihuang/lab3/lab3_2b.jl");
-
-function printAscii(x::Array)
-    x = uint8(x);
-    @printf(STDOUT,"%s",ascii(x));
+function writeASCII(x::ASCIIString,file_name)
+        s = open(file_name,"r+");
+        write(s,x);         #write the ASCIIString to file
+        close(s);
 end
 
-function writeASCII(x::Array,file_name)
-	open(file_name,"r+");
-	x = uint8(x);
-	x = ascii(x);
-        #writecsv(file_name,x);
+function readASCII(file_name)
+        s = open(file_name,"r");
+        readline(s);
+        close(s);
 end
 
-function writeArray(x::Array,file_name)
-	open(file_name,"r+");
-	writecsv(file_name,x);
-end
-
-x = 127*rand(1024);
-open("/Users/feifeihuang/lab3_2b.txt","r+");
-#writeASCII(x,"lab3_2b.txt");
-#writeArray(x,"lab3_2b.txt");
