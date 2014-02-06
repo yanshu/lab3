c5dbdee53dcf6bccfb7581bf1d569b7a561ea1ca
yanshu <huangfeifei91@gmail.com>
2014-02-06 18:35:48 -0500

    problem 2d

--- lab3_2d.jl
+++ lab3_2d.jl
@@ -1,11 +1,7 @@
 function writeBinary(x::Array,file_name)
         s = open(file_name,"w+");
         n = length(x);
-        y = Array(ASCIIString,n);
-        for i = 1:n
-                y[i] = bin(x[i]);
-        end
-        writecsv(file_name,y)
+        write(s,x);
         close(s);
 end
 
@@ -16,10 +12,9 @@ function readBinary(file_name)
 end
 
 function getTime()
-    x = 127*rand(10);	#create an array of random number from 0 to 127
-    x = int(x);
+    x = 127*rand(1024);	#create an array of random number from 0 to 127
+    x = uint8(x);
     file_name = "/Users/feifeihuang/lab3/file_binary.txt";
-    print(x);
     println("time to write binary to file = ", @elapsed writeBinary(x,file_name));
     println("time to read binary from file = ", @elapsed readBinary(file_name));
     println("time to read binary from file = ", @elapsed readBinary(file_name));
