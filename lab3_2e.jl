ec6ba37d10db1c73bf342bfc5f7cc062f322ffc9
yanshu <huangfeifei91@gmail.com>
2014-02-07 10:07:17 -0500

    update

--- lab3_2e.jl
+++ lab3_2e.jl
@@ -2,27 +2,29 @@ Pkg.add("HDF5")
 using HDF5, JLD
 
 function writeHDF5(x::Array,file_name)
+        @assert(isa(x,Array));
         #@save file_name x
         file = jldopen(file_name,"w");
         @write file x;
         close(file);
-
 end
 
 function readHDF5(file_name)
-        @load file_name x ;
+        file = jldopen(file_name,"r");
+        read(file);
+        close(file);
 end
 
 function getTime()
     x = 127*rand(1024);	#create an array of random number from 0 to 127
     x = uint8(x);
     file_name = "/Users/feifeihuang/lab3/file_hdf5.jld";
-    @save file_name x;
-    @load file_name x;
+    writeHDF5(x,file_name);
+    readHDF5(file_name);
     println("time to write in hdf5 to file = ", @elapsed writeHDF5(x,file_name));
     println("time to read in hdf5 from file = ", @elapsed readHDF5(file_name));
     println("time to read in hdf5 from file = ", @elapsed readHDF5(file_name));
     println("time to read in hdf5 from file = ", @elapsed readHDF5(file_name));
 end
 
-getTime()
+#getTime()
