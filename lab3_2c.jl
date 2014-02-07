a4a4af621af4c4d3659769f2ac295cdb4590480a
yanshu <huangfeifei91@gmail.com>
2014-02-06 20:54:36 -0500

    update

--- lab3_2c.jl
+++ lab3_2c.jl
@@ -2,7 +2,7 @@ include("/Users/feifeihuang/lab3/lab3_2a.jl");
 include("/Users/feifeihuang/lab3/lab3_2b.jl");
 
 function getArrayAndTime()
-    x = 127*rand(1024*1024);	#create an array of random number from 0 to 127
+    x = 127*rand(1024);	#create an array of random number from 0 to 127
     file_name = "/Users/feifeihuang/lab3/file_ascii.txt";
     println("\n","time to print ASCII to STDOUT = ", @elapsed printSTDOUT(convertToAscii(x)));
     println("\n","time to write ASCII to file = ", @elapsed writeASCII(x,file_name));
